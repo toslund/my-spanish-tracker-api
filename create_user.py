beac32613ee243a4b3217e1e8b676f119b14e423
toslund <timoslund@gmail.com>
2022-01-01 14:46:53 -0800

    create user one at a time via cli instead of csv

--- create_user.py
+++ create_user.py
@@ -11,17 +11,18 @@ logger = logging.getLogger(__name__)
 def main() -> None:
     logger.info("Creating user")
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--inputcsv")
+    parser.add_argument("-n", "--name")
+    parser.add_argument("-e", "--email")
+    parser.add_argument("-p", "--password")
     args = vars(parser.parse_args())
-    input_csv = args['inputcsv']
-    db = SessionLocal()
-    if input_csv:
-        with open(input_csv, newline='') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                created, user = create_user(db, fullname=row['name'], email=row['email'], password=row['password'], uuid=uuid.uuid4())
-                print(f'created user: {user.email}' if created == True else f'user already exists: {user.email}')
+    name = args['name']
+    email = args['email']
+    password = args['password']
+    if name and email and password:
+        db = SessionLocal()
+        created, user = create_user(db, fullname=name, email=email, password=password, uuid=uuid.uuid4())
+        print(f'created user: {user.email}' if created == True else f'user already exists: {user.email}')
     else:
-        logger.info("CSV not supplied")
+        logger.info("inputs not properly supplied")
 if __name__ == "__main__":
     main()
